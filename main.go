bdc8c1cf28aa14114e4a6bb5e2517ca3827edc1a
Michał Adamczyk <michal.adamczyk.1990@gmail.com>
2021-04-28 23:14:28 +0200

    [FEAT] Working example of the program
    This program already works as I expect it to.
    It alternates between two time counters for
    work and rest and indicates it with different
    emojis.
    
    It works on UDS sockets to communicate with
    other programs on the local host, e.g., tmux.

--- main.go
+++ main.go
@@ -6,41 +6,130 @@ that you want to spend 25 minutes working and then take a five-minute break.
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net"
-	"strings"
+	"os"
+	"time"
 )
 
-// TODO: I want to have a server running on Unix Domain Sockets or UDS for short
+const (
+	USAGE = `usage:
+   wtime
+   wtime -work=25m -rest=5m
+   wtime -work=3h30m43s
 
-const SOCK = "/tmp/wtime.sock"
+params:
+`
+	SOCK   = "/tmp/wtime.sock"
+	TICK   = time.Second
+	EMOJIS = "💣😴"
+)
 
 func main() {
-}
-
-type Server interface {
-	Run() error
-	Close() error
-}
-
-type ServerUDS struct {
-	addr string
-	serv net.Listener
-}
-
-func (s *ServerUDS) Run() error   { return fmt.Errorf("") }
-func (s *ServerUDS) Close() error { return fmt.Errorf("") }
-
-// Create fresh server with the specified protocol and address.
-// The addr attribute is loosely understood and here it corresponds
-// to a file name on the localhost. This is the way UDS works.
-func NewServer(prot, addr string) (Server, error) {
-	switch strings.ToLower(prot) {
-	case "unix":
-		return &ServerUDS{
-			addr: addr,
-		}, nil
-	case "tcp":
+	var (
+		protocol          string
+		addr              string
+		work, rest, emoji string
+	)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, USAGE)
+		flag.PrintDefaults()
 	}
-	return nil, fmt.Errorf("unsupported protocal given")
+	flag.StringVar(&protocol, "protocol", "unix", "server protocol")
+	flag.StringVar(&addr, "addr", SOCK, "socket address")
+	flag.StringVar(&work, "work", "25m", "time for work")
+	flag.StringVar(&rest, "rest", "5m", "time for work")
+	flag.StringVar(&emoji, "emoji", EMOJIS, "rotate these emojis")
+	flag.Parse()
+
+	s, err := NewServer(protocol, addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "wtime %s", err)
+		os.Exit(1)
+	}
+	defer s.Close()
+
+	wDur, err := time.ParseDuration(work)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "wtime: %s", err)
+		os.Exit(2)
+	}
+	rDur, err := time.ParseDuration(rest)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "wtime: %s", err)
+		os.Exit(2)
+	}
+
+	if err := s.Run(wDur, rDur); err != nil {
+		fmt.Fprintf(os.Stderr, "wtime %s", err)
+	}
+}
+
+// UDS server layout.
+type Server struct {
+	addr, protocol string
+	serv           net.Listener
+}
+
+// Run launches the server that now listens for connections.
+func (s *Server) Run(w, r time.Duration) (err error) {
+	s.serv, err = net.Listen(s.protocol, s.addr)
+	for {
+		conn, err := s.serv.Accept()
+		if err != nil {
+			err = fmt.Errorf("unable to establish connection")
+			break
+		}
+		if conn == nil {
+			err = fmt.Errorf("unable to communicate")
+			break
+		}
+		go HandleConn(conn, w, r)
+	}
+	return
+}
+
+// Close the listener server.
+func (s *Server) Close() {
+	s.serv.Close()
+}
+
+// Transmit the message to connected client(s).
+func HandleConn(conn net.Conn, w, r time.Duration) {
+	defer conn.Close()
+	emojis := []rune(EMOJIS)
+	countdown(conn, w, emojis[0])
+	countdown(conn, r, emojis[1])
+}
+
+func countdown(conn net.Conn, d time.Duration, e rune) {
+	timer := time.NewTimer(d)
+	ticker := time.NewTicker(TICK)
+
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			d -= time.Duration(TICK)
+			_, err := io.WriteString(conn, string(e)+" "+d.String()+"\n")
+			if err != nil {
+				break loop
+			}
+		case <-timer.C:
+			break loop
+		}
+	}
+}
+
+// NewServer returns a fresh instance of a server on a Unix Domain Socket.
+func NewServer(protocol, addr string) (Server, error) {
+	if err := os.RemoveAll(addr); err != nil {
+		return Server{}, err
+	}
+	return Server{
+		addr:     addr,
+		protocol: protocol,
+	}, nil
 }
